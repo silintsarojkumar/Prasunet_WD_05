836a8e6d7ead0fb029809f9c266df42e8e131f9e
saroj kumar <128085396+silintsarojkumar@users.noreply.github.com>
2024-07-07 23:49:18 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -9,22 +9,22 @@ const createWeatherCard=(cityName,weatherItem,index)=>{
         return `
                 <div class="details">
                     <h3>${cityName} ${weatherItem.dt_txt.split(" ")[0]}</h3>
-                    <h6>Temperature:${(weatherItem.main.temp -273.15).toFixed(2)}°C</h6>
-                    <h6>Wind:${weatherItem.wind.speed} M/s</h6>
-                    <h6>Humidity:${weatherItem.main.humidity}%</h6>
+                    <h6>Temperature: ${(weatherItem.main.temp -273.15).toFixed(2)}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/s</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}%</h6>
                 </div>
                 <div class="icon">
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-                    <h6>${weatherItem.weather[0].description}</h6>
+                    <h6> ${weatherItem.weather[0].description}</h6>
                 </div>`;
 
     } else{
         return `<li class="card">    
-        <h5>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+        <h5> (${weatherItem.dt_txt.split(" ")[0]})</h3>
         <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
-        <h6>Temp:${(weatherItem.main.temp -273.15).toFixed(2)}°C</h4>
-        <h6>Wind:${weatherItem.wind.speed} M/s</h4>
-        <h6>Humidity:${weatherItem.main.humidity}%</h4>
+        <h6>Temp: ${(weatherItem.main.temp -273.15).toFixed(2)}°C</h4>
+        <h6>Wind: ${weatherItem.wind.speed} M/s</h4>
+        <h6>Humidity: ${weatherItem.main.humidity}%</h4>
         </li>`;
 
     }
@@ -32,7 +32,7 @@ const createWeatherCard=(cityName,weatherItem,index)=>{
 
 }
 const getWeatherDetails = ( cityName,lat,lon)=>{
-    const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=993e41354632e44530065345d7ee12db`;
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=993e41354632e44530065345d7ee12db`;
     fetch(WEATHER_API_URL).then(res => res.json()).then(data =>{
         const uniqueForecastDays = [];
         const fiveDaysForecast = data.list.filter(forecast => {
@@ -64,7 +64,7 @@ const getWeatherDetails = ( cityName,lat,lon)=>{
 const getCityCoordinates = () => {
     const cityName = cityInput.value.trim();
     if(!cityName) return;
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=993e41354632e44530065345d7ee12db`;
+    const GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=993e41354632e44530065345d7ee12db`;
 
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
@@ -80,7 +80,7 @@ const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
         position => {
             const {latitude, longitude} = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=993e41354632e44530065345d7ee12db`;
+            const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=993e41354632e44530065345d7ee12db`;
             fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data => {
                 const {name}=data[0];
                 getWeatherDetails( name,latitude,longitude);
